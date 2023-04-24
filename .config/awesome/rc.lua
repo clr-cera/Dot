1be3c2e4cc450158a12e1dde18ec3db573a25da1
clr-cera <93736542+clr-cera@users.noreply.github.com>
2023-04-23 21:02:35 -0300

    some fixes

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -450,7 +450,6 @@ end)
 awful.spawn.with_shell("xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --mode 2560x1080 --pos 0x0 --rotate normal --output DP-2 --primary --mode 1920x1080 --pos 2560x0 --rotate normal")
 awful.spawn.with_shell("nitrogen --restore")
 awful.spawn.with_shell("picom --backend glx")
-awful.spawn.with_shell("dunst")
 
 beautiful.useless_gap = 5
 --Configurations
