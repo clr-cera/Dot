9748cd4dacfcff5e130918b671123c22c419f58a
clr-cera <93736542+clr-cera@users.noreply.github.com>
2023-05-19 20:33:03 -0300

    Return to picom original fork

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -458,7 +458,7 @@ end)
 awful.spawn.with_shell(
   "xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --mode 2560x1080 --pos 0x0 --rotate normal --output DP-2 --primary --mode 1920x1080 --pos 2560x0 --rotate normal")
 awful.spawn.with_shell("nitrogen --restore")
-awful.spawn.with_shell("picom --experimental-backends")
+awful.spawn.with_shell("picom")
 
 --Configurations
 beautiful.useless_gap = 5
--- .config/fish/functions/picoms.fish
+++ .config/fish/functions/picoms.fish
@@ -1,4 +1,4 @@
 function picoms --wraps='picom --experimental-backends & disown' --description 'alias picoms=picom experimental-backends & disown'
-  picom --experimental-backends & disown $argv
+  picom & disown $argv
         
 end
