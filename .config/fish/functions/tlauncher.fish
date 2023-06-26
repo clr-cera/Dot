e6caeb72f0ed339c79404ad750d9ef238e59f7f1
clr-cera <93736542+clr-cera@users.noreply.github.com>
2023-06-26 20:19:01 -0300

    v1.00
    true release....

--- .config/fish/functions/tlauncher.fish
+++
@@ -1,4 +0,0 @@
-function tlauncher --wraps='sudo java -jar /home/nix/Games/Tlauncher/TLauncher-2.885.jar' --description 'alias tlauncher=sudo java -jar /home/nix/Games/Tlauncher/TLauncher-2.885.jar'
-  java -jar /home/nix/Games/Tlauncher/TLauncher-2.885.jar $argv
-        
-end
--- .config/fish/functions/victoria.fish
+++
@@ -1,4 +0,0 @@
-function victoria --wraps=/home/nix/Games/Victoria.3-jc141/start.n.sh --description 'alias victoria=/home/nix/Games/Victoria.3-jc141/start.n.sh'
-  /home/nix/Games/Victoria.3-jc141/start.n.sh $argv
-        
-end
